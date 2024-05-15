34f7277b873fc04132dda0c893328001cc6236d9
Herzanich Georgii <109143250+Fenix524@users.noreply.github.com>
2024-05-15 23:51:18 +0200

    update
    додав поля:
     - totalWater (Загальна кількість випитої води за день)
     - waterRecordsAmount (Кількість порцій доданих в цей день)
    
    Змнив swager
    Переробив protect

--- midleWares/protect.js
+++ midleWares/protect.js
@@ -2,6 +2,7 @@ import HttpErrors from '../helpers/HttpError.js'
 import { User } from '../models/userModel.js'
 import { asyncWrapper } from './asyncWrapper.js'
 import { checkToken } from '../services/jwtService.js'
+import { checkTokenPlusUser } from '../services/userService.js'
 
 export const protect = asyncWrapper(async (req, res, next) => {
   const token =
@@ -11,8 +12,12 @@ export const protect = asyncWrapper(async (req, res, next) => {
   const userId = checkToken(token)
   if (!userId) return next(HttpErrors(401))
 
-  const currentUser = await User.findById(userId)
+  const currentUser = await checkTokenPlusUser(userId, token)
+
+  // const currentUser = await User.findById(userId)
   if (!currentUser) return next(HttpErrors(401))
+
+  // checkTokenPlusUser()
 
   req.user = currentUser
 
