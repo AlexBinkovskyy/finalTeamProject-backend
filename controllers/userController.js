b56752b5f99d1ebdad798f82faeab96f570d414d
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-18 21:33:36 +0300

    fix caching issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,8 +35,8 @@ export const sendVerificationEmail = async (req, res, next) => {
   const user = await checkUserByEmail(req.body);
   console.log(user);
   if (!user) throw HttpError(404, "User not found");
-  if (!user.email ) throw HttpError(400, "missing required field email");
-  if(user.isVerified) throw HttpError(401)
+  if (!user.email) throw HttpError(400, "missing required field email");
+  if (user.isVerified) throw HttpError(401);
 
   await emailService(user);
 
@@ -86,21 +86,30 @@ export const upload = async (req, res, next) => {
 export const chahgeUserCreds = async (req, res, next) => {
   const updatedUser = { ...req.user._doc, ...req.body };
   req.user = await updateUser(updatedUser);
-  res.status(201).json({
-    token: req.user.token,
-    user: {
-      _id: req.user._id,
-      name: req.user.name,
-      email: req.user.email,
-      gender: req.user.gender,
-      dailyNorma: req.user.dailyNorma,
-      weight: req.user.weight,
-      activeTime: req.user.activeTime,
-      goal: req.user.goal,
-      avatarUrl: req.user.avatarUrl,
-      isVerified: req.user.isVerified,
-    },
-  });
+  res
+    .status(201)
+    .json({
+      token: req.user.token,
+      user: {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+        gender: req.user.gender,
+        dailyNorma: req.user.dailyNorma,
+        weight: req.user.weight,
+        activeTime: req.user.activeTime,
+        goal: req.user.goal,
+        avatarUrl: req.user.avatarUrl,
+        isVerified: req.user.isVerified,
+      },
+    })
+    .set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    )
+    .set("Pragma", "no-cache")
+    .set("Expires", "0")
+    .set("Surrogate-Control", "no-store");
 };
 
 export const emailPassRecoveryController = async (req, res, nex) => {
@@ -122,22 +131,18 @@ export const recoveryPasswordController = async (req, res, next) => {
     const user = await checkResetTokenPlusUser(id, resetToken);
     if (!user) throw HttpError(401, "Not authorized");
 
-   const updatedUser = await changeUserPassword(user, password)
-   res.status(201).json({
-    message: "Password changed successfuly",
-    user: {
-      _id: updatedUser._id,
-      email: updatedUser.email,
-      isVerified: updatedUser.isVerified
-    }
-   })
+    const updatedUser = await changeUserPassword(user, password);
+    res.status(201).json({
+      message: "Password changed successfuly",
+      user: {
+        _id: updatedUser._id,
+        email: updatedUser.email,
+        isVerified: updatedUser.isVerified,
+      },
+    });
   } catch (error) {
     next(error);
   }
-
-
-  
- 
 };
 
 export const getAllUsers = async (req, res, next) => {
