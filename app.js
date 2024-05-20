acbe0fc3cafa80a4e0f95a1be31fd3c3c5e1780c
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-21 01:53:58 +0300

    some

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
+  next();
+});
+
+
 app.use("/api/users", userRouter);
 app.use("/api/water", waterRouter);
 app.use("/api/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
